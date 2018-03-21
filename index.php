da07efccfa778dc0b03edbb34f8af5af619c5c6c
gustavonaxx <37569478+gustavonaxx@users.noreply.github.com>
2018-03-20 21:04:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	if(isset($_COOKIE['log']) && $_COOKIE['log'] == Criptografar(1)) {
 		require_once 'temp/home.php';
 	}else {
-		echo '<script>location.href="login";</script>';
+		echo d'<script>location.href="login";</script>';
 	}
 
 
